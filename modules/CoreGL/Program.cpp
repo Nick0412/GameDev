0ad8777b49fc3e2a9050dab5f34f3b885b5c44e5
Nicholas Draper <nickd2225@gmail.com>
2020-12-30 14:59:32 -0800

    fixed lniking issue with shader

--- modules/CoreGL/Program.cpp
+++ modules/CoreGL/Program.cpp
@@ -4,12 +4,12 @@ namespace CoreGL
 {
 
 #ifdef SHADER_DIR
-    static const std::string g_shader_path = SHADER_DIR;
+    const std::string g_shader_path = SHADER_DIR;
 #else
-    static const std::string g_shader_path = "UNDEFINED";
+    const std::string g_shader_path = "UNDEFINED";
 #endif
 
-    static std::string readShaderFile(const std::string& file)
+    std::string readShaderFile(const std::string& file)
     {
         std::string file_text;
         std::ifstream file_stream;
@@ -31,7 +31,7 @@ namespace CoreGL
         return file_text;
     }
 
-    static GLuint createShader(GLenum shader_type, const std::string& shader_file)
+    GLuint createShader(GLenum shader_type, const std::string& shader_file)
     {
         GLuint shader_id = glCreateShader(shader_type);
         std::string full_shader_file = g_shader_path + shader_file;
@@ -42,7 +42,7 @@ namespace CoreGL
         return shader_id;
     }
 
-    static void checkShaderCompileStatus(GLuint shader_id)
+    void checkShaderCompileStatus(GLuint shader_id)
     {
         GLint result;
         glGetShaderiv(shader_id, GL_COMPILE_STATUS, &result);
@@ -58,7 +58,7 @@ namespace CoreGL
     Program::Program()
     : m_program_id(glCreateProgram())
     {
-        
+
     }
 
     void Program::attachShader(GLenum shader_type, const std::string& shader_file) const
@@ -66,12 +66,14 @@ namespace CoreGL
         GLuint shader_id = createShader(shader_type, shader_file);
         glCompileShader(shader_id);
         checkShaderCompileStatus(shader_id);
+        glAttachShader(m_program_id, shader_id);
     }
 
     void Program::linkProgram()
     {   
         glLinkProgram(m_program_id);
         std::cout << " TESTING 1: " + m_program_id << "\n";
+        std::cout << "ERROR " << glGetError() << "\n";
         checkProgramLinkStatus();
         std::cout << " Testing 2: " + m_program_id << "\n";
         glValidateProgram(m_program_id);
@@ -84,10 +86,13 @@ namespace CoreGL
 
     void Program::checkProgramLinkStatus()
     {
+        glLinkProgram(m_program_id);
         GLint result;
         glGetProgramiv(m_program_id, GL_LINK_STATUS, &result);
+        std::cout << "ERROR 1" << glGetError() << "\n";
         if (result == GL_FALSE)
         {
+            std::cout << "ERROR 2" << glGetError() << "\n";
             char info_log[1024];
             glGetProgramInfoLog(m_program_id, 1024, nullptr, info_log);
             auto ex = std::string("Program Link Error: ") + info_log;
